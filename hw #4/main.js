7250ae5b698c6c16e65ef0404148e18e42110f8e
Maria Vagradian <vagradian.masha@pdffiller.team>
2023-10-16 19:24:36 +0300

    homeworks #4 const name changed

--- hw #4/main.js
+++ hw #4/main.js
@@ -1,11 +1,11 @@
 // Створити скрипт для додавання, віднімання, множення та поділу двох чисел та виведення результатів.
 // Користувач вводить два числа через два промпти, і повідомлення виводиться алертом з таким результатом:
-const number1 = Number(prompt('Enter your number'));
-const number2 = Number(prompt('Enter your number'));
+const firstNumber = Number(prompt('Enter your number'));
+const secondNumber = Number(prompt('Enter your number'));
 const mathResults = `
-${number1} + ${number2} = ${number1 + number2}\n
-${number1} - ${number2} = ${number1 - number2}\n
-${number1} * ${number2} = ${number1 * number2}\n
-${number1} / ${number2} = ${number1 / number2}`
+${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}\n
+${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}\n
+${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}\n
+${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`
 
 alert(mathResults);
