fdc59f952468c5c39bb42b0217ec6cd7e0fdf362
Maria Vagradian <vagradian.masha@pdffiller.team>
2023-10-27 21:32:19 +0300

    homework 12 in progress

--- hw #11/main.js
+++ hw #11/main.js
@@ -1,4 +1,5 @@
 // Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+// Наприклад:
 // const array = [1, 2, 3, 4, 5, 6, 7];
 // removeElement(array, 5 );
 // console.log(array);
---
+++ hw #12/main.js
@@ -0,0 +1,14 @@
+// Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length.
+// Наприклад:
+// const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+// const key = generateKey(16, characters);
+// console.log(key); // eg599gb60q926j8i
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+
+function generateKey (length, characters) {
+
+}
+
+console.log(key);
