d4b3aae23582e6c119c20ef290eab6b7bc3c2876
Maria Vagradian <vagradian.masha@pdffiller.team>
2023-10-30 13:49:34 +0200

    homework 12

--- hw #12/main.js
+++ hw #12/main.js
@@ -8,7 +8,15 @@ const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 const key = generateKey(16, characters);
 
 function generateKey (length, characters) {
+  let key = '';
+  const charactersLength = characters.length;
 
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    key += characters.charAt(randomIndex);
+  }
+
+  return key;
 }
 
 console.log(key);
