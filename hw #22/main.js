e7dd4a06227a2786aea721d1eb644e03c3b14836
Maria Vagradian <vagradian.masha@pdffiller.team>
2023-12-26 19:03:57 +0200

    homework 24

--- hw #22/main.js
+++ hw #22/main.js
@@ -1,20 +1,37 @@
-// Дано 3 блоки
-// В лівій частині сторінки - перелік категорій.
-// При натисканні на категорію в середній блок виводиться список товарів цієї категорії.
-// Клік на товар - інформацію про товар у правому блоці.
-// В інформації товару - кнопка “купити”.
-// При натисканні на “купити” з'являється повідомлення, що товар куплений і повернення у вихідний стан програми ( коли відображається лише список категорій).
 document.addEventListener('DOMContentLoaded', function () {
   const categoryList = document.querySelector('.categoryList');
+  const categories = document.querySelector('.categories');
   const productList = document.querySelector('.productList');
+  const products = document.querySelector('.products');
   const infoText = document.querySelector('.infoText');
+  const info = document.querySelector('.info');
   const buyButton = document.querySelector('.action');
+  const myOrdersBtn = document.querySelector('.actionOrders');
+  const ordersContainer = document.querySelector('.ordersContainer');
+  const ordersList = document.querySelector('.ordersList');
 
   const productsData = {
     Fruits: ['Apple', 'Banana', 'Lemon'],
     Vegetables: ['Potato', 'Tomato', 'Cucumber'],
     Drinks: ['Coke', 'Sprite', 'Fanta']
   };
+
+  function calculatePrice(product) {
+    // Replace this with your actual price calculation logic
+    const prices = {
+      'Apple': 1.00,
+      'Banana': 0.75,
+      'Lemon': 0.50,
+      'Potato': 1.25,
+      'Tomato': 1.50,
+      'Cucumber': 1.00,
+      'Coke': 2.00,
+      'Sprite': 2.00,
+      'Fanta': 1.75
+    };
+
+    return prices[product] || 0.00;
+  }
 
   function createCategoryItem(category) {
     const li = document.createElement('li');
@@ -49,6 +66,13 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentProduct = product;
 
     buyButton.addEventListener('click', function buyProduct() {
+      const order = {
+        product: currentProduct,
+        date: new Date().toLocaleString(),
+        price: calculatePrice(currentProduct) // Assuming you have a function to calculate the price
+      };
+
+      saveOrder(order);
       alert(`You've bought ${currentProduct}`);
       productList.innerHTML = '';
       infoText.textContent = '';
@@ -57,4 +81,62 @@ document.addEventListener('DOMContentLoaded', function () {
       buyButton.removeEventListener('click', buyProduct);
     });
   }
+
+  function saveOrder(order) {
+    let orders = JSON.parse(localStorage.getItem('orders')) || [];
+    orders.push(order);
+    localStorage.setItem('orders', JSON.stringify(orders));
+  }
+
+  function displayOrderDetails(order) {
+    alert(`Order details:\nProduct: ${order.product}\nDate: ${order.date}\nPrice: $${order.price}`);
+  }
+
+  myOrdersBtn.addEventListener('click', function () {
+    categories.style.display = 'none';
+    products.style.display = 'none';
+    info.style.display = 'none';
+    ordersContainer.style.display = 'block';
+    loadOrders();
+  });
+
+
+  function createOrderItem(order) {
+    const li = document.createElement('li');
+    li.textContent = `${order.date} - $${order.price}`;
+
+    // Add a delete button for each order
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.classList.add('action', 'actionDelete');
+    deleteButton.addEventListener('click', () => deleteOrder(order));
+
+    li.appendChild(deleteButton);
+    return li;
+  }
+
+  function loadOrders() {
+    const orders = JSON.parse(localStorage.getItem('orders')) || [];
+    ordersList.innerHTML = '';
+
+    orders.forEach(order => {
+      const orderItem = createOrderItem(order);
+      ordersList.appendChild(orderItem);
+    });
+  }
+
+  function deleteOrder(orderToDelete) {
+    let orders = JSON.parse(localStorage.getItem('orders')) || [];
+    orders = orders.filter(order => order.date !== orderToDelete.date);
+    localStorage.setItem('orders', JSON.stringify(orders));
+    loadOrders();
+  }
+
+  myOrdersBtn.addEventListener('click', function () {
+    categoryList.classList.add('hide');
+    productList.classList.add('hide');
+    infoText.classList.add('hide');
+    ordersContainer.style.display = 'block';
+    loadOrders();
+  });
 });
